7eb368f61546e373dc4706826c33bbe3ea120d65
BANGWON-LEE <qkddnjs31@gmail.com>
2024-09-26 23:58:47 +0900

    edit : 코드 구조 변경으로 타입 interface 수정

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,15 +1,16 @@
-export interface CalendarDayType {
-  allDates: Date[];
-  currentMonth: string;
-}
 export interface DayInfoType {
   start: number;
   end: number;
 }
 export interface CalendarDaysUIType {
   arrDayState: object[][] | string[][] | undefined;
-  currentMonth: string;
+
   dateRange: DayInfoType;
   btnDisabled: string;
   toggleDay: (day: object | string) => void;
 }
+
+export const initialDayInfo: DayInfoType = {
+  start: 0,
+  end: 0,
+};
