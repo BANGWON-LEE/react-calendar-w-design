caf6b8e2d487a88b84777ed63ff763695f1a20cf
BANGWON LEE <qkddnjs31@gmail.com>
2024-11-10 20:15:26 +0900

    edit : 불필요한 코드 제거

--- src/components/input/InputText.tsx
+++ src/components/input/InputText.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { DateRangeContextType } from '../../type/contextType'
 // import {DateRange}
 import { DateRangeContext } from '../../context/DateRangeProvider'
