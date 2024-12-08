c3afa784f0b1ae2397b94bf36fa761dabbec54b2
BANGWON LEE <qkddnjs31@gmail.com>
2024-12-08 19:05:30 +0900

    edit : 초기값 재설정

--- src/context/DayProvider.tsx
+++ src/context/DayProvider.tsx
@@ -1,24 +1,22 @@
-import React, { createContext, useState } from "react";
-import { DayContextType } from "../type/contextType";
+import React, { createContext, useState } from 'react'
+import { DayContextType } from '../type/contextType'
 
 // 1. Context 생성
-
-
 
 export const DayContext = createContext<DayContextType>({
   allDates: [new Date()],
   setAllDates: () => {},
-});
+})
 
 // 2. Provider 컴포넌트
 const DayProvider = ({ children }: { children: React.ReactNode }) => {
-  const [allDates, setAllDates] = useState<(number | Date)[]>([new Date()]);
+  const [allDates, setAllDates] = useState<(number | Date)[]>([])
 
   return (
     <DayContext.Provider value={{ allDates, setAllDates }}>
       {children}
     </DayContext.Provider>
-  );
-};
+  )
+}
 
-export default DayProvider;
+export default DayProvider
